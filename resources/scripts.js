ce237063bfa7b4aef82325a766f8f989c96cd95e
B <supernerbd@yahoo.de>
2015-09-28 05:13:57 +0200

    last commit. Everything should work now. Version 1.0! :)

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,47 +1,41 @@
 //project specific scripts
 
 "use strict";
-
-
 //variables
-
-//
 var showMenu=false;
-//var controls=false;
 var width=window.innerWidth;
 var height=window.innerHeight;
 var NUM_SAMPLES = 256;
-var SOUND_1 = 'media/Fake Orchestra.mp3'; //'media/New Adventure Theme.mp3';
-var SOUND_2 = 'media/Flesh Maze Tango.mp3'; //'media/Peanuts Theme.mp3';
-var SOUND_3 = 'media/Peper Steak'; //'media/The Picard Song.mp3';
-var audioElement;
-var analyserNode;
+var MAXRADIUS = 200;
+var SOUND_1 = 'media/Fake Orchestra.mp3'; 
+var SOUND_2 = 'media/Flesh Maze Tango.mp3'; 
+var SOUND_3 = 'media/Peper Steak'; 
+var audioElement, delayNode, analyserNode;
 var delayAmount=0;
-var delayNode;
 var canvas,ctx;
-var background=false;
+var backgroundPic=false;
+var backgroundColor="black";
 var img;
 // pixel manip vars
 var tintRed = false, tintBlue = false, tintGreen = false;
 var noise = false, lines = false, bwNoise = false;
 //animation vars
-var mCircle = false, mSquare = true, circle = true, clines = false, invert = false;
-var maxRadius=200;
-
-var data, dataW;
-var percent;
-
+var mCircle = false, mSquare = true, circle = true, clines = true, invert = false;
+var data, dataW, percent;
 //interactive vars
+var clickEnabled=true, controlsOn=false;
+// mouse control vars
+
 var mouse;
 var cCounter=0;
-var clicks = new Array();
+var clicks = new Array(); //init array
 for (var i=0; i<20; i++){
 	clicks[i]=0;
 }
 //Init and resize
 
 
-function init(){
+function init(){ //init canvas, audio, animation and ui
 	document.querySelector('#canvas').width=width; //resize the canvas drawing area.
 	document.querySelector('#canvas').height=height;
 	canvas = document.querySelector ('#canvas');
@@ -51,21 +45,22 @@ function init(){
 	animation();
 }
 window.onload = init;
-//window.onresize = init;
 
 
 //UI
-function setupUI(){
-		canvas.addEventListener("mousedown", function(e){
-		mouse = getMouse(e);
-		clicks[cCounter]=mouse.x;
-		cCounter++;
-		clicks[cCounter]=mouse.y;
-		cCounter++;
-		if(cCounter>=20){
-			cCounter=0;
-			for (var i=0; i<20; i++){
-				clicks[i]=0;
+function setupUI(){ //setup all eventListener and eventHandler
+		canvas.addEventListener("mousedown", function(e){ //mouse control
+		if (clickEnabled){
+			mouse = getMouse(e);
+			clicks[cCounter]=mouse.x;
+			cCounter++;
+			clicks[cCounter]=mouse.y;
+			cCounter++;
+			if(cCounter>=20){ //Max 10 clicks possible after that it is reset to 0.
+				cCounter=0;
+				for (var i=0; i<20; i++){
+					clicks[i]=0;
+				}
 			}
 		}
 	});
@@ -73,16 +68,38 @@ function setupUI(){
 		playStream(audioElement,e.target.value);
 	};
 	document.querySelector("#backgroundSelect").onchange = function(e){
-		if (e.target.value=="none"){background=false;}
+		if (e.target.value=="none"){backgroundPic=false;}
 		else{
-		background=true;
+		backgroundPic=true;
 		img=document.getElementById(e.target.value);}
 	};
-		document.querySelector("#fsButton").onclick = function(){
+	document.querySelector("#clickButton").onclick = function(){
+		if (clickEnabled){
+			clickEnabled=false;
+		}
+		else{
+			clickEnabled=true;
+		}
+	}
+	document.querySelector("#controlsButton").onclick = function(){
+		if (controlsOn) {
+			document.querySelector("#controls").style.setProperty("display","none");
+			document.querySelector("#controls").style.setProperty("display","none");
+			controlsOn=false;
+		}
+		else{
+			document.querySelector("#controls").style.setProperty("display","inherit");
+			controlsOn=true;
+		}
+	}
+	document.querySelector("#backgroundSelectColor").onchange = function(e){
+		backgroundColor=e.target.value;
+	};
+	document.querySelector("#fsButton").onclick = function(){
 		requestFullscreen(canvas);
 	};
 	document.querySelector("#radius").onchange = function(e){
-		maxRadius=e.target.value;
+		MAXRADIUS=e.target.value;
 	};
 	document.querySelector("#red").onchange = function(e){
 		if(e.target.checked){
@@ -179,30 +196,30 @@ function setupUI(){
 	};
 }
  // Full Screen
-		function requestFullscreen(element) {
-			if (element.requestFullscreen) {
-			  element.requestFullscreen();
-			} else if (element.mozRequestFullscreen) {
-			  element.mozRequestFullscreen();
-			} else if (element.mozRequestFullScreen) { // camel-cased 'S' was changed to 's' in spec
-			  element.mozRequestFullScreen();
-			} else if (element.webkitRequestFullscreen) {
-			  element.webkitRequestFullscreen();
-			}
-			// .. and do nothing if the method is not supported
-		};
+function requestFullscreen(element) {
+	if (element.requestFullscreen) {
+	  element.requestFullscreen();
+	} else if (element.mozRequestFullscreen) {
+	  element.mozRequestFullscreen();
+	} else if (element.mozRequestFullScreen) { // camel-cased 'S' was changed to 's' in spec
+	  element.mozRequestFullScreen();
+	} else if (element.webkitRequestFullscreen) {
+	  element.webkitRequestFullscreen();
+	}
+	// .. and do nothing if the method is not supported
+};
 
 //SOUND AND AUDIO
 
 
-function initAudio(){
+function initAudio(){ //init audio
 	audioElement = document.querySelector('audio');
 	//Analyser
 	analyserNode = createWebAudioContextWithAnalyserNode(audioElement);
 	//start playing
 	playStream(audioElement,SOUND_1);
 }
-function createWebAudioContextWithAnalyserNode(audioElement) {
+function createWebAudioContextWithAnalyserNode(audioElement) { //create audio notes connections
 	var audioCtx, analyserNode, sourceNode;
 	// create new AudioContext
 	audioCtx = new (window.AudioContext || window.webkitAudioContext);
@@ -227,7 +244,7 @@ function createWebAudioContextWithAnalyserNode(audioElement) {
 	analyserNode.connect(audioCtx.destination);
 	return analyserNode;
 }
-function playStream(audioElement,path){
+function playStream(audioElement,path){ //playing audio streams
 	audioElement.src = path;
 	audioElement.play();
 	audioElement.volume = 0.2;
@@ -237,13 +254,13 @@ function playStream(audioElement,path){
 //DRAWINGS
 
 
-function animation(){
+function animation(){ //animations on canvas
 	requestAnimationFrame(animation); // this schedules a call to the animation() method in 1/60 seconds
 	data = new Uint8Array(NUM_SAMPLES/2); // create a new array of 8-bit integers (0-255)
 	analyserNode.getByteFrequencyData(data); // populate the array with the frequency data. notice these arrays can be passed "by reference" 	
 	
 	// drawings
-	ctx.fillStyle="black";
+	ctx.fillStyle=backgroundColor;
 	ctx.fillRect(0,0,width,height); 
 	var barWidth = Math.floor(width/data.length);
 	var barSpacing = 1;
@@ -255,9 +272,9 @@ function animation(){
 		analyserNode.getByteTimeDomainData(dataW);
 		var barWidthW = Math.floor(width/dataW.length);
 		for(var i=0; i<dataW.length; i++) { 
-			dataW[i]=dataW[i]*1.5;
+			dataW[i]=dataW[i]*1.5; 
 			ctx.save();
-			ctx.strokeStyle=drawGradients();//"red";
+			ctx.strokeStyle=drawGradients();
 			ctx.lineWidth=barWidth;
 			ctx.beginPath();
 			ctx.translate(0, canvas.height);
@@ -270,35 +287,29 @@ function animation(){
 		}
 	}
 	//Background only when selected
-	if (background==true){
+	if (backgroundPic==true){
 		ctx.drawImage(img,0,0);}
 	//Rest
 	for(var i=0; i<data.length; i++) { // loop through the data and draw!
 		ctx.fillStyle = 'rgba(0,255,0,0.4)';
 		ctx.strokeStyle= 'rgba(0,255,0,0.4)';
 		percent = data[i]/255;
-		
-		// the higher the amplitude of the sample (bin) the taller the bar
-		// remember we have to draw our bars left-to-right and top-down
-		//ctx.fillRect(i * (barWidth + barSpacing),topSpacing + 256-data[i],barWidth,barHeight); 
-		// draw inverted bars
-		//ctx.fillRect(640-i* (barWidth + barSpacing),topSpacing+256-data[i]-20,barWidth,barHeight);
 			
 		//circles in the middle
 		if(mCircle){
 			//red-ish circles
-			var circleRadius = percent*maxRadius;
+			var circleRadius = percent*MAXRADIUS;
 			
 			ctx.save();
-			drawCircles(canvas.width/2,canvas.height/2,percent,maxRadius, getColor(255, 111, 111, .34-percent/3.0), 'rgba(0,0,0,0)');
+			drawCircles(canvas.width/2,canvas.height/2,percent,MAXRADIUS, getColor(255, 111, 111, .34-percent/3.0), 'rgba(0,0,0,0)');
 			ctx.restore();
 			//blue-ish circles, bigger, more transparent
 			ctx.save();
-			drawCircles(canvas.width/2,canvas.height/2,percent,maxRadius*1.5, getColor(0,0,255, .10-percent/10.0), 'rgba(0,0,0,0)');
+			drawCircles(canvas.width/2,canvas.height/2,percent,MAXRADIUS*1.5, getColor(0,0,255, .10-percent/10.0), 'rgba(0,0,0,0)');
 			ctx.restore();
 			//yellow-ish circles, smaller
 			ctx.save();
-			drawCircles(canvas.width/2, canvas.height/2, percent, maxRadius*.50, getColor(200,200,0,.5-percent/5.0),'rgba(0,0,0,0)');
+			drawCircles(canvas.width/2, canvas.height/2, percent, MAXRADIUS*.50, getColor(200,200,0,.5-percent/5.0),'rgba(0,0,0,0)');
 			ctx.restore();
 		}
 		//Squares
@@ -342,35 +353,18 @@ function animation(){
 				drawCircles(0,0,1,10, 'rgba(0,0,0,0)', 'rgba(0,255,0,1)');
 				ctx.restore();
 		}
-		//mouse fucntions
-		for (var j=0; j<=clicks.length; j++){
-			if(j%2==0 && clicks[j]!=0 && clicks[j+1]!=0){
-				drawCircles(clicks[j], clicks[j+1], percent, maxRadius, getColor(Math.floor((Math.random() * 255) +1),0,Math.floor((Math.random() * 255) +1),.39 * percent/6), getColor(Math.floor((Math.random() * 255) +1),0,Math.floor((Math.random() * 255) +1),.39 * percent/6));
-			}
+		//mouse fucntion
+		//draw circle at each x and y in array
+		for (var j=0; j<=clicks.length; j++){ //goes through array with click x's and y's
+			if(j%2==0 && clicks[j]!=0 && clicks[j+1]!=0){ //if even number and x and y are not 0
+				drawCircles(clicks[j], clicks[j+1], percent, MAXRADIUS, getColor(Math.floor((Math.random() * 255) +1),0,Math.floor((Math.random() * 255) +1),.39 * percent/6), getColor(Math.floor((Math.random() * 255) +1),0,Math.floor((Math.random() * 255) +1),.39 * percent/6));
+			} //clicks[j]==x, clicks[j+1]==y
 		}
 	}
 	manipulatePixels(); 
 } 
-//drawing functions; used by several functions
-
-/*function drawCurve(data){
-		ctx.save();
-    	ctx.lineWidth="3";
-		ctx.strokeStyle = "black";
-		ctx.beginPath();
-		
-		ctx.moveTo(0, 0);
-		ctx.quadraticCurveTo(data, data, width, height);
-		ctx.stroke();
-		
-		ctx.strokeStyle = "red";
-		ctx.beginPath();
-		ctx.moveTo(0, 0);
-		ctx.bezierCurveTo(5, 150, 50, 300, width, height);
-		ctx.stroke();
-		ctx.restore();
-    }*/
-function drawCircles(x, y, percent, maxRadius, fillStyle, strokeStyle){
+// function for mouse control
+function drawCircles(x, y, percent, maxRadius, fillStyle, strokeStyle){ //draw circle on x and y of each of the mouse clicks
 	var circleRadius = percent * maxRadius;
 	ctx.beginPath();
 	ctx.fillStyle = fillStyle;
@@ -380,14 +374,7 @@ function drawCircles(x, y, percent, maxRadius, fillStyle, strokeStyle){
 	ctx.stroke();
 	ctx.closePath;
 }
-function drawSquares(x, y, percent, maxSize, fillStyle){
-	var rectSize = percent * maxSize;
-	ctx.fillStyle = fillStyle;
-	ctx.fillRect(x,y, rectSize, rectSize);
-	ctx.fillRect(x,y, -rectSize, -rectSize);
-	ctx.fillRect(x,y, rectSize, -rectSize);
-	ctx.fillRect(x,y, -rectSize, rectSize);	
-}
+//Gradient
 function drawGradients() {
 		var grad = ctx.createLinearGradient(0,0,width,height);
 		grad.addColorStop(0, 'red');
--- resources/utilities.js
+++ resources/utilities.js
@@ -1,18 +1,18 @@
 //general functions
 "use strict";
 
-function getMouse(e){
+function getMouse(e){ //get mouse position
 	var mouse = {}
 	mouse.x = e.pageX - e.target.offsetLeft;
 	mouse.y = e.pageY - e.target.offsetTop;
 	return mouse;	
 }
 
-function getRandom(min,max){
+function getRandom(min,max){ //get randome number between min and max
 	return Math.random() * (max-min) + min;
 }
 
-function getColor(red, green, blue, alpha){
+function getColor(red, green, blue, alpha){ //get rgba color
 	var color='rgba('+red+','+green+','+blue+', '+alpha+')';
 	return color;	
 }
